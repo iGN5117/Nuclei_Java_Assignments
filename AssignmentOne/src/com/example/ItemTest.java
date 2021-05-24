75cda98a2fbcd160d1260c7c64e5e8e129c840c1
Nilesh Gajwani <nilesh.gajwani@gonuclei.com>
2021-05-24 12:40:46 +0530

    pushed assignment one

---
+++ AssignmentOne/src/com/example/ItemTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ItemTest {
+    private Item itemRaw;
+    private Item itemManufactured;
+    private Item itemImported;
+
+    @Before
+    public void init() {
+        itemRaw = new Item("Raw_Item", 10.4, 2, ItemType.Raw);
+        itemManufactured = new Item("Manufactured_Item", 100.5, 10, ItemType.Manufactured);
+        itemImported = new Item("Imported_Item", 200, 20, ItemType.Imported);
+    }
+
+    @Test
+    @DisplayName("Test to check if tax per item is calculated properly")
+    public void testTaxCalculation() {
+        Assertions.assertEquals(itemRaw.calculateTax(), 16.44, "Raw item tax calculation should be proper");
+        Assertions.assertEquals(itemManufactured.calculateTax(), 120.55, "Manufactured item tax calculation should be proper");
+        Assertions.assertEquals(itemImported.calculateTax(), 231, "Imported item tax calculation should be proper");
+    }
+
+    @Test
+    @DisplayName("Test to check if item details are printed")
+    public void testItemDetailsPrinting() {
+        itemRaw.printItemDetails();
+        itemManufactured.printItemDetails();
+        itemImported.printItemDetails();
+    }
+}
